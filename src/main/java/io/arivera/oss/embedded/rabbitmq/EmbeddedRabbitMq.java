4024b6429d551a0d5c510da2d72454edb1560787
Alejandro Rivera <alejandro.rivera.lopez@gmail.com>
2016-08-10 19:33:05 +0800

    Extracted remaining logic for running+stopping into separate components

--- src/main/java/io/arivera/oss/embedded/rabbitmq/EmbeddedRabbitMq.java
+++ src/main/java/io/arivera/oss/embedded/rabbitmq/EmbeddedRabbitMq.java
@@ -1,37 +1,15 @@
 package io.arivera.oss.embedded.rabbitmq;
 
-import io.arivera.oss.embedded.rabbitmq.util.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.zeroturnaround.exec.ProcessExecutor;
-import org.zeroturnaround.exec.ProcessResult;
 import org.zeroturnaround.exec.StartedProcess;
-import org.zeroturnaround.exec.stream.slf4j.Slf4jOutputStream;
-import org.zeroturnaround.exec.stream.slf4j.Slf4jStream;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 
 public class EmbeddedRabbitMq {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(EmbeddedRabbitMq.class);
-
   private EmbeddedRabbitMqConfig config;
   private StartedProcess rabbitMqProcess;
-  private PublishingProcessListener rabbitMqProcessListener;
 
   public EmbeddedRabbitMq(EmbeddedRabbitMqConfig config) {
     this.config = config;
-  }
-
-  public EmbeddedRabbitMqConfig getConfig() {
-    return config;
   }
 
   public void start() throws DownloadException, ProcessException {
@@ -40,15 +18,15 @@ public class EmbeddedRabbitMq {
     run();
   }
 
-  private void download() {
-    Runnable downloader = new Downloader(this.getConfig());
+  private void download() throws DownloadException {
+    Runnable downloader = new Downloader(config);
     if (config.shouldCachedDownload()) {
       downloader = new CachedDownloader(downloader, config);
     }
     downloader.run();
   }
 
-  private void extract() {
+  private void extract() throws DownloadException {
     Runnable extractor = new Extractor(config);
     if (config.shouldCachedDownload()) {
       extractor = new CachedExtractor(extractor, config);
@@ -57,92 +35,14 @@ public class EmbeddedRabbitMq {
   }
 
   private void run() throws ProcessException {
-    String command = config.getAppFolder().toString() + "/sbin/rabbitmq-server";
-    try {
-      PatternFinderOutputStream initializationWatcher = new PatternFinderOutputStream(".*completed with \\d+ plugins.*");
-      rabbitMqProcessListener = new PublishingProcessListener();
-      rabbitMqProcessListener.addSubscriber(initializationWatcher);
-
-      Slf4jOutputStream processLogger = new RabbitMqServerProcessLogger("rabbitmq-server");
-
-      Slf4jOutputStream processOutputStream = Slf4jStream.of(EmbeddedRabbitMq.class).asInfo();
-      rabbitMqProcess = new ProcessExecutor()
-          .directory(config.getAppFolder())
-          .command(command)
-          .redirectError(processLogger)
-          .redirectOutput(processLogger)
-          .redirectOutputAlsoTo(initializationWatcher)
-          .addListener(new LoggingProcessListenerDecorator(processOutputStream.getLogger(), rabbitMqProcessListener))
-          .destroyOnExit()
-          .start();
-
-      boolean match = initializationWatcher.waitForMatch(config.getRabbitMqServerInitializationTimeoutInMillis(), TimeUnit.MILLISECONDS);
-      if (!match) {
-        throw new ProcessException("Could not start RabbitMQ Server. See logs for more details.");
-      }
-    } catch (IOException e) {
-      throw new ProcessException("Could not execute RabbitMQ rabbitMqProcess", e);
-    }
+    rabbitMqProcess = new Starter(config).call();
   }
-
 
   public void stop() throws ShutDownException {
-    List<String> command = Arrays.asList( config.getAppFolder() + "/sbin/rabbitmqctl", "stop");
-    try {
-      Slf4jStream loggingStream = Slf4jStream.of(EmbeddedRabbitMq.class, "Process.rabbitmqctl");
-
-      ProcessResult rabbitMqCtlProcessResult = new ProcessExecutor()
-          .directory(config.getAppFolder())
-          .command(command)
-          .redirectError(loggingStream.asError())
-          .redirectOutput(loggingStream.asInfo())
-          .addListener(new LoggingProcessListener(loggingStream.asDebug().getLogger()))
-          .destroyOnExit()
-          .start()
-          .getFuture()
-          .get(config.getDefaultRabbitMqCtlTimeoutInMillis(), TimeUnit.MILLISECONDS);
-
-      int exitValue = rabbitMqCtlProcessResult.getExitValue();
-      if (exitValue == 0) {
-        LOGGER.info("Submitted command to stop RabbitMQ Server successfully.");
-      } else {
-        LOGGER.warn("Command '"+ StringUtils.join(command, " ")+"' exited with value: " + exitValue);
-      }
-    } catch (IOException e) {
-      throw new ShutDownException("Could not successfully execute: " + StringUtils.join(command, " "), e);
-    } catch (InterruptedException | ExecutionException | TimeoutException e) {
-      throw new ShutDownException("Command '" + StringUtils.join(command, " ") + "' did not finish as expected", e);
+    if (rabbitMqProcess == null) {
+      throw new IllegalStateException("Stop shouldn't be called unless 'start()' was successful.");
     }
-
-    try {
-      Future<ProcessResult> processfuture = rabbitMqProcess.getFuture();
-      ProcessResult rabbitMqProcessResult = processfuture.get(config.getDefaultRabbitMqCtlTimeoutInMillis(), TimeUnit.MILLISECONDS);
-      int exitValue = rabbitMqProcessResult.getExitValue();
-      if (exitValue == 0) {
-        LOGGER.info("RabbitMQ Server stopped successfully.");
-      } else {
-        LOGGER.warn("RabbitMQ Server stopped with exit value: " + exitValue);
-      }
-    } catch (InterruptedException | ExecutionException | TimeoutException e) {
-      throw new ShutDownException("Error while waiting for RabbitMQ Server to shut down", e);
-    }
-
-  }
-
-  private static class RabbitMqServerProcessLogger extends Slf4jOutputStream {
-
-    public RabbitMqServerProcessLogger(String processName) {
-      super(LoggerFactory.getLogger(EmbeddedRabbitMq.class.getName() + ".Process." + processName));
-    }
-
-    @Override
-    protected void processLine(String line) {
-      if (line.startsWith("ERROR:")) {
-        log.error(line);
-      } else {
-        log.info(line);
-      }
-    }
+    new Stopper(config, rabbitMqProcess).run();
   }
 
 }
---
+++ src/main/java/io/arivera/oss/embedded/rabbitmq/Stopper.java
@@ -0,0 +1,80 @@
+package io.arivera.oss.embedded.rabbitmq;
+
+import io.arivera.oss.embedded.rabbitmq.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.zeroturnaround.exec.ProcessExecutor;
+import org.zeroturnaround.exec.ProcessResult;
+import org.zeroturnaround.exec.StartedProcess;
+import org.zeroturnaround.exec.stream.slf4j.Slf4jStream;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+class Stopper implements Runnable {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(Stopper.class);
+
+  private final EmbeddedRabbitMqConfig config;
+  private final StartedProcess rabbitMqProcess;
+
+  Stopper(EmbeddedRabbitMqConfig config, StartedProcess rabbitMqProcess) {
+    this.config = config;
+    this.rabbitMqProcess = rabbitMqProcess;
+  }
+
+  @Override
+  public void run() throws ShutDownException {
+    stopUsingRabbitMqCtl();
+    waitForRabbitMqServerToFinish();
+  }
+
+  private void stopUsingRabbitMqCtl() {
+    List<String> command = Arrays.asList( config.getAppFolder() + "/sbin/rabbitmqctl", "stop");
+    try {
+      Slf4jStream loggingStream = Slf4jStream.of(EmbeddedRabbitMq.class, "Process.rabbitmqctl");
+
+      ProcessResult rabbitMqCtlProcessResult = new ProcessExecutor()
+          .directory(config.getAppFolder())
+          .command(command)
+          .redirectError(loggingStream.asError())
+          .redirectOutput(loggingStream.asInfo())
+          .addListener(new LoggingProcessListener(loggingStream.asDebug().getLogger()))
+          .destroyOnExit()
+          .start()
+          .getFuture()
+          .get(config.getDefaultRabbitMqCtlTimeoutInMillis(), TimeUnit.MILLISECONDS);
+
+      int exitValue = rabbitMqCtlProcessResult.getExitValue();
+      if (exitValue == 0) {
+        LOGGER.info("Submitted command to stop RabbitMQ Server successfully.");
+      } else {
+        LOGGER.warn("Command '"+ StringUtils.join(command, " ")+"' exited with value: " + exitValue);
+      }
+    } catch (IOException e) {
+      throw new ShutDownException("Could not successfully execute: " + StringUtils.join(command, " "), e);
+    } catch (InterruptedException | ExecutionException | TimeoutException e) {
+      throw new ShutDownException("Command '" + StringUtils.join(command, " ") + "' did not finish as expected", e);
+    }
+  }
+
+  private void waitForRabbitMqServerToFinish() {
+    try {
+      Future<ProcessResult> processfuture = rabbitMqProcess.getFuture();
+      ProcessResult rabbitMqProcessResult = processfuture.get(config.getDefaultRabbitMqCtlTimeoutInMillis(), TimeUnit.MILLISECONDS);
+      int exitValue = rabbitMqProcessResult.getExitValue();
+      if (exitValue == 0) {
+        LOGGER.info("RabbitMQ Server stopped successfully.");
+      } else {
+        LOGGER.warn("RabbitMQ Server stopped with exit value: " + exitValue);
+      }
+    } catch (InterruptedException | ExecutionException | TimeoutException e) {
+      throw new ShutDownException("Error while waiting for RabbitMQ Server to shut down", e);
+    }
+  }
+}
