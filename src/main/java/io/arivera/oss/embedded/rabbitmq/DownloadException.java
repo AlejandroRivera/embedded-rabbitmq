8e2628c1d312f864948ea1f9bd20e42943a037ea
Alejandro Rivera <alejandro.rivera.lopez@gmail.com>
2016-08-12 16:30:37 +0800

    Adding support for Zip files (Windows downloads)

--- src/main/java/io/arivera/oss/embedded/rabbitmq/DownloadException.java
+++ src/main/java/io/arivera/oss/embedded/rabbitmq/DownloadException.java
@@ -2,6 +2,10 @@ package io.arivera.oss.embedded.rabbitmq;
 
 public class DownloadException extends RuntimeException {
 
+  public DownloadException(String msg){
+    super(msg);
+  }
+
   public DownloadException(String msg, Throwable cause) {
     super(msg, cause);
   }
