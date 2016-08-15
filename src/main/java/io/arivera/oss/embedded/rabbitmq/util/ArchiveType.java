3e467d7a9b3424e1fa66eefe5c78bb5af8546764
Alejandro Rivera <alejandro.rivera.lopez@gmail.com>
2016-08-15 18:08:38 +0800

    Fixed new FindBugs issues.

--- src/main/java/io/arivera/oss/embedded/rabbitmq/util/ArchiveType.java
+++ src/main/java/io/arivera/oss/embedded/rabbitmq/util/ArchiveType.java
@@ -1,10 +1,12 @@
 package io.arivera.oss.embedded.rabbitmq.util;
 
+import java.util.Locale;
+
 public enum ArchiveType {
 
   TAR_GZ, TAR_XZ;
 
   public String getExtension() {
-    return name().toLowerCase().replace("_", ".");
+    return name().toLowerCase(Locale.US).replace("_", ".");
   }
 }
