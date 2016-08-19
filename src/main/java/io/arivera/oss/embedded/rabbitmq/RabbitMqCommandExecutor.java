e6906e314afb383c4014c41efb963cd646e1501d
Alejandro Rivera <alejandro.rivera.lopez@gmail.com>
2016-08-19 17:30:25 +0800

    Created a wrapper for `rabbitmq-server`, which allows for streaming of output

--- src/main/java/io/arivera/oss/embedded/rabbitmq/RabbitMqCommandExecutor.java
+++ src/main/java/io/arivera/oss/embedded/rabbitmq/RabbitMqCommandExecutor.java
@@ -2,12 +2,16 @@ package io.arivera.oss.embedded.rabbitmq;
 
 import io.arivera.oss.embedded.rabbitmq.util.SystemUtils;
 
+import org.apache.commons.io.output.NullOutputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.zeroturnaround.exec.ProcessExecutor;
 import org.zeroturnaround.exec.StartedProcess;
 import org.zeroturnaround.exec.stream.slf4j.Slf4jStream;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,7 +19,7 @@ import java.util.concurrent.Callable;
 
 public class RabbitMqCommandExecutor implements Callable<StartedProcess> {
 
-  private static final String LOGGER_NAME_TEMPLATE = "Process.%s";
+  private static final String LOGGER_TEMPLATE = "%s.Process.%s";
 
   private static final String EXECUTABLE_FOLDER = "sbin";
   private static final String COMMAND_TEMPLATE = "%s%s%s%s";
@@ -25,9 +29,11 @@ public class RabbitMqCommandExecutor implements Callable<StartedProcess> {
   private static final String UNIT_EXT = "";
 
   private final EmbeddedRabbitMqConfig config;
-  private final String command;
   private final String executableCommand;
   private final List<String> arguments;
+
+  private Logger processOutputLogger;
+  private OutputStream outputStream;
 
   /**
    * @param config the configuration information used to launch the process with the correct context.
@@ -37,11 +43,27 @@ public class RabbitMqCommandExecutor implements Callable<StartedProcess> {
    *                  "{@code ./rabbitmq-plugins enable foo}", utilize {@code ["enable", "foo"]} as value
    */
   public RabbitMqCommandExecutor(EmbeddedRabbitMqConfig config, String command, String... arguments) {
-    this.config = config;
     String extension = IS_WIN ? WIN_EXT : UNIT_EXT;
-    this.command = command;
+    this.config = config;
     this.executableCommand = String.format(COMMAND_TEMPLATE, EXECUTABLE_FOLDER, File.separator, command, extension);
     this.arguments = Arrays.asList(arguments);
+
+    this.outputStream = new NullOutputStream();
+    this.processOutputLogger = LoggerFactory.getLogger(
+        String.format(LOGGER_TEMPLATE, EmbeddedRabbitMq.class.getName(), command));
+  }
+
+  /**
+   * Output from the process will be written here as it happens.
+   */
+  public RabbitMqCommandExecutor captureOutput(OutputStream outputStream) {
+    this.outputStream = outputStream;
+    return this;
+  }
+
+  public RabbitMqCommandExecutor loggingWith(Logger logger) {
+    this.processOutputLogger = logger;
+    return this;
   }
 
   @Override
@@ -50,22 +72,20 @@ public class RabbitMqCommandExecutor implements Callable<StartedProcess> {
     List<String> fullCommand = new ArrayList<>(arguments);
     fullCommand.add(0, executableCommand);
 
+    Slf4jStream loggingStream = Slf4jStream.of(processOutputLogger);
+    LoggingProcessListener loggingListener = new LoggingProcessListener(processOutputLogger);
+
     ProcessExecutor processExecutor = new ProcessExecutor()
         .environment(config.getEnvVars())
         .directory(config.getAppFolder())
         .command(fullCommand)
-        .destroyOnExit();
+        .destroyOnExit()
+        .addListener(loggingListener)               // Logs process events (like start, stop...)
+        .redirectError(loggingStream.asError())     // Logging for output made to STDERR
+        .redirectOutput(loggingStream.asInfo())     // Logging for output made to STDOUT
+        .redirectOutputAlsoTo(outputStream);
 
-    processExecutor = configureListeners(processExecutor);
     return processExecutor.start();
   }
 
-  private ProcessExecutor configureListeners(ProcessExecutor executor) {
-    Slf4jStream loggingStream = Slf4jStream.of(EmbeddedRabbitMq.class, String.format(LOGGER_NAME_TEMPLATE, command));
-    LoggingProcessListener loggingListener = new LoggingProcessListener(loggingStream.asDebug().getLogger());
-
-    return executor.addListener(loggingListener)
-        .redirectError(loggingStream.asError())
-        .redirectOutput(loggingStream.asInfo());
-  }
 }
---
+++ src/main/java/io/arivera/oss/embedded/rabbitmq/RabbitMqServer.java
@@ -0,0 +1,67 @@
+package io.arivera.oss.embedded.rabbitmq;
+
+import org.apache.commons.io.output.NullOutputStream;
+import org.zeroturnaround.exec.ProcessResult;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.concurrent.Future;
+
+/**
+ * A wrapper for the command "<code>{@value #COMMAND}</code>", used for starting the RabbitMQ broker.
+ */
+public class RabbitMqServer {
+
+  public static final String COMMAND = "rabbitmq-server";
+
+  private final EmbeddedRabbitMqConfig config;
+  private final OutputStream outputStream;
+
+  public RabbitMqServer(EmbeddedRabbitMqConfig config) {
+    this(config, new NullOutputStream());
+  }
+
+  public RabbitMqServer(EmbeddedRabbitMqConfig config, OutputStream outputStream) {
+    this.config = config;
+    this.outputStream = outputStream;
+  }
+
+  /**
+   * Starts the RabbitMQ Server and keeps the process running until it's stopped.
+   * <p>
+   * Running rabbitmq-server in the foreground displays a banner message, and reports on progress in the startup
+   * sequence, concluding with the message "{@code completed with [N] plugins.}", indicating that the
+   * RabbitMQ broker has been started successfully.
+   * <p>
+   * To read the output, either:
+   * <ul>
+   *   <li>
+   *     wait for the Future to finish and use {@link ProcessResult} output methods, or
+   *   </li>
+   *   <li>
+   *     provide an Output Stream through the {@link RabbitMqServer#RabbitMqServer(EmbeddedRabbitMqConfig, OutputStream) constructor}
+   *     to receive it as it happens.
+   *   </li>
+   * </ul>
+   */
+  public Future<ProcessResult> start() throws IOException {
+    return execute();
+  }
+
+  /**
+   * Start the RabbitMq Server in a detached state.
+   * <p>
+   * This means the process will exit immediately and no PID file will be written to file.
+   */
+  public Future<ProcessResult> startDetached() throws IOException {
+    return execute("-detached");
+  }
+
+  public Future<ProcessResult> execute(String... arguments) throws IOException {
+    return new RabbitMqCommandExecutor(config, COMMAND, arguments)
+        .captureOutput(outputStream)
+        .call()
+        .getFuture();
+  }
+
+}
