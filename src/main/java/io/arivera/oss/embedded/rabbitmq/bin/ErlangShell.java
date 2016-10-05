ee5f7d5a343c73ee8968737895dce2481d06fb58
Michael Laccetti <michael@laccetti.com>
2016-10-05 11:23:13 -0400

    We cannot fork the `erl` command, due to shell madness. It is "easier" to compile a simple Erlang app...

--- src/main/java/io/arivera/oss/embedded/rabbitmq/bin/ErlangShell.java
+++ src/main/java/io/arivera/oss/embedded/rabbitmq/bin/ErlangShell.java
@@ -1,22 +1,26 @@
 package io.arivera.oss.embedded.rabbitmq.bin;
 
-import io.arivera.oss.embedded.rabbitmq.EmbeddedRabbitMqConfig;
-import io.arivera.oss.embedded.rabbitmq.util.StringUtils;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
+import io.arivera.oss.embedded.rabbitmq.EmbeddedRabbitMqConfig;
+import io.arivera.oss.embedded.rabbitmq.apache.commons.io.FileUtils;
+import io.arivera.oss.embedded.rabbitmq.util.StringUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.output.NullOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zeroturnaround.exec.ProcessExecutor;
 import org.zeroturnaround.exec.ProcessResult;
-import org.zeroturnaround.exec.StartedProcess;
 import org.zeroturnaround.exec.stream.slf4j.Level;
 import org.zeroturnaround.exec.stream.slf4j.Slf4jStream;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
 /**
  * A wrapper for the command "<code>{@value ErlangShell#COMMAND}</code>", used for checking/testing the Erlang version.
@@ -46,46 +50,46 @@ public class ErlangShell {
    * or available on the path.
    */
   public void checkErlangExistence() throws ErlangShellException {
-    int exitVal;
+    final ProcessResult result = execute();
+    final int exitVal = result.getExitValue();
+    final String results = result.outputUTF8();
 
-    try {
-      final ErlangShell shell = new ErlangShell(config);
-      final Future<ProcessResult> future = shell.execute();
-      final ProcessResult result = future.get();
-      exitVal = result.getExitValue();
-    } catch (final InterruptedException | ExecutionException ex) {
-      throw new ErlangShellException("Could not start/execute Erlang shell.", ex);
-    }
-
-    if (exitVal != 0) {
-      throw new ErlangShellException("Could not retrieve Erlang version.");
+    if (exitVal != 0 || results.toLowerCase().contains("not found")) {
+      throw new ErlangShellException("Erlang not found");
     }
   }
 
-  private Future<ProcessResult> execute() throws RuntimeException {
-    final List<String> fullCommand = Arrays.asList("erl", "-eval", "'{ok, Version} = "
-        + "file:read_file(filename:join([code:root_dir(), \"releases\", "
-        + "erlang:system_info(otp_release), \"OTP_VERSION\"])), "
-        + "erlang:display(erlang:binary_to_list(Version)), halt().'", "-noshell");
+  private ProcessResult execute() throws RuntimeException {
+    if (!config.getAppFolder().mkdirs()) {
+      throw new ErlangShellException("Could not create temporary directory.");
+    }
+
+    final File outputFile = new File(config.getAppFolder(), "test.erl");
+
+    try {
+      final InputStream inputStream = getClass().getResourceAsStream("/test.erl");
+      FileUtils.copyInputStreamToFile(inputStream, outputFile);
+    } catch (final IOException ie) {
+      throw new ErlangShellException("Could not create temporary file.", ie);
+    }
 
     final Slf4jStream loggingStream = Slf4jStream.of(processOutputLogger);
 
-    final ProcessExecutor processExecutor = processExecutorFactory.createInstance()
-        .environment(config.getEnvVars())
-        .directory(config.getAppFolder())
-        .command(fullCommand)
-        .destroyOnExit()
-        .redirectError(loggingStream.as(Level.WARN))     // Logging for output made to STDERR
-        .redirectOutput(loggingStream.as(Level.INFO))     // Logging for output made to STDOUT
-        .redirectOutputAlsoTo(new NullOutputStream())         // Pipe stdout to this stream for the application to process
-        .redirectErrorAlsoTo(new NullOutputStream())     // Pipe stderr to this stream for the application to process
-        .readOutput(true);                   // Store the output in the ProcessResult as well.
+    final ProcessExecutor processExecutor = new ProcessExecutor()
+      .directory(config.getAppFolder())
+      .command("erlc", "test.erl")
+      .timeout(10L, TimeUnit.SECONDS)
+      .redirectError(loggingStream.as(Level.WARN))     // Logging for output made to STDERR
+      .redirectOutput(loggingStream.as(Level.INFO))     // Logging for output made to STDOUT
+      .redirectOutputAlsoTo(new NullOutputStream())         // Pipe stdout to this stream for the application to process
+      .redirectErrorAlsoTo(new NullOutputStream())     // Pipe stderr to this stream for the application to process
+      .destroyOnExit()
+      .readOutput(true);
 
     try {
-      final StartedProcess startedProcess = processExecutor.start();
-      return startedProcess.getFuture();
-    } catch (IOException e) {
-      throw new RabbitMqCommandException("Failed to execute: " + StringUtils.join(fullCommand, " "), e);
+      return processExecutor.execute();
+    } catch (final IOException | InterruptedException | TimeoutException ex) {
+      throw new ErlangShellException("Failed to execute: " + StringUtils.join(processExecutor.getCommand(), " "), ex);
     }
   }
 }
